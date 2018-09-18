8988c3999176820830c81439049bbef463e79a9f
SIMATIT MUNI <simatitcse@gmail.com>
2018-09-18 18:52:32 +0530

    Data-Analytics_Session-8_Assignment-3

---
+++ 8.3.R
@@ -0,0 +1,28 @@
+# 1. A recent national study showed that approximately 44.7% of college students have used Wikipedia as a
+#    source in at least one of their term papers. Let X equal the number of students in a random sample of
+#    size n = 31 who have used Wikipedia as a source.
+
+# Perform the below functions
+
+# a. Find the probability that X is equal to 17.
+dbinom(17, size = 31, prob = 0.447)
+# 0.07532248
+
+# b. Find the probability that X is at most 13.
+pbinom(13, size = 31, prob = 0.447)
+# 0.451357
+
+# c. Find the probability that X is bigger than 11.
+pbinom(11, size = 31, prob = 0.447, lower.tail = FALSE)
+# 0.8020339
+
+# d. Find the probability that X is at least 15.
+pbinom(14, size = 31, prob = 0.447, lower.tail = FALSE)
+# 0.406024
+
+# e. Find the probability that X is between 16 and 19, inclusive.
+sum(dbinom(16:19, size = 31, prob = 0.447))
+# 0.2544758
+
+diff(pbinom(c(19, 15), size = 31, prob = 0.447, lower.tail = FALSE))
+# 0.2544758
